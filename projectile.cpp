81ed7859b70b69194b68b0c3449669a67a8b64a3
shivaniwaz <31860942+shivaniwaz@users.noreply.github.com>
2017-10-11 17:15:38 -0400

    Update projectile.cpp

--- projectile.cpp
+++ projectile.cpp
@@ -1,16 +1,32 @@
 #include "projectile.hpp"
 
+
 const double m = 1;
 const double g = 9.8066;
 const double dt = 0.001;
+const double q= 1;
+const double k= 0.5;
+const double w =2;
+const double Eo=1;
 
-auto force(TState s) { return VecR2<double>{0, -m * g}; }
 
-auto euler_step(TState s, VecR2<double> accel) {
+auto force(TState s) {
+  return (q*(Eo/sqrt(2))*(VecR3<double>{cos((k*s.position.z)-(w*s.t)),sin((k*s.position.z)-(w*s.t)),0.}));
+}
+
+auto euler_step(TState s, VecR3<double> accel){
   TState next;
   next.t = s.t + dt;
   next.position = s.position + (s.velocity * dt);
-  next.velocity = s.velocity + (accel * dt);
+  next.velocity = s.velocity + (accel* dt);
+  return next;
+}
+
+auto verlet_step(TState s,VecR3<double>accel){
+  TState next;
+  next.t = s.t + dt;
+  next.position = s.position + (s.velocity * dt)+(0.5*(accel*dt*dt));
+  next.velocity = s.velocity + ((((accel)+(force(next)/m))/2.00)*dt);
   return next;
 }
 
@@ -21,13 +37,13 @@ void n_steps(unsigned n, TState state0) {
   else {
     auto state = state0;
     for (unsigned k = 0; k < n; ++k) {
-      state = euler_step(state, force(state) / m);
+      state = verlet_step(state, force(state)/ m);
       print_tstate(state);
     }
   }
 }
 
 int main() {
-  n_steps(1200, TState{0., {0, 0.1}, {5, 5}});
+  n_steps(7000, TState{0., {((-Eo)/(w*w)*sqrt(2)),0,0}, {0, (Eo/(w*sqrt(2))),-0.25}});
   return 0;
 }
